7eecc32ddcda81646f8ee7a90a5fda9a1f90c829
pengzl <411240333@qq.com>
2022-03-19 23:01:29 +0800

    add bst max min floor celling method.

--- java-code/src/main/java/com/learn/algorithm/binarytree/Node.java
+++ java-code/src/main/java/com/learn/algorithm/binarytree/Node.java
@@ -4,8 +4,8 @@ public class Node<K extends Comparable<K>,V> {
     public K key;
     public V value;
     public int size;
-    public Node left;
-    public Node right;
+    public Node<K, V> left;
+    public Node<K, V> right;
 
     public Node(K key, V vlaue, int size, Node left, Node right) {
         this.key = key;
